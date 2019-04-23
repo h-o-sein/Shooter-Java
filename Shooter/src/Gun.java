ccae03739f4b89d3e331c52b2c68bb00e7b3d9ed
Hosein <enjoohosein@gmail.com>
2019-04-23 18:32:51 +0430

    Update Class Gun

--- Shooter/src/Gun.java
+++ Shooter/src/Gun.java
@@ -1,2 +1,17 @@
 public class Gun {
+
+    private int x;
+    private int y;
+    private int Width;
+    private int Height;
+
+    public Gun(int x,int y,int Width, int Height) {
+
+        this.x = x;
+        this.y = y;
+        this.Height = Height;
+        this.Width = Width;
+
+    }
+
 }
