d88ea4f1ff50c4e5e49a7aa744194b1a295106a9
Hosein <enjoohosein@gmail.com>
2019-04-23 17:17:57 +0430

    Update

---
+++ Shooter/src/Game.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JPanel {
+
+    public Game() {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+
+
+    }
+}
