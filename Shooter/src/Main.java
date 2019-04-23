68d1795fa0d542f7995953bcf7fccf85bb9666df
Hosein <enjoohosein@gmail.com>
2019-04-23 17:38:36 +0430

    Update Main

--- Shooter/src/Main.java
+++ Shooter/src/Main.java
@@ -1,10 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Shooter Game");
+
+        Game game = new Game();
+        game.setPreferredSize(new Dimension(game.WIDTH,game.HEIGHT));
+        frame.add(game);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
 
 
     }
