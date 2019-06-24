98fcbcae51653d0bedefcdb40f989eb48cba7c94
achd75 <49908160+achd75@users.noreply.github.com>
2019-06-24 22:01:19 +0430

    Final Ali Chizfahm Daneshmandian

--- Shooter/src/Birds.java
+++ Shooter/src/Birds.java
@@ -60,4 +60,6 @@ public class Birds {
         return BirdRect;
     }
 
+
+
 }
--- Shooter/src/Main.java
+++ Shooter/src/Main.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Main {
+public class Main extends JFrame {
 
     public static void main(String[] args) {
 
@@ -16,6 +18,64 @@ public class Main {
         frame.setVisible(true);
 
 
+    }}
+
+
+  /*  private JButton buttonLeft;
+
+    public Main(){
+        super();
+
+        setLayout(new FlowLayout());
+
+        buttonLeft = new JButton("Left");
+        add(buttonLeft);
+
+        buttonLeft.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+
     }
 
+
 }
+*/
+/*
+class DrawPanel extends JPanel {
+
+
+    protected void paintComponent(Graphics ui) {
+        super.paintComponent(ui);
+
+        setBackground(Color.WHITE);
+
+        ui.setColor(Color.RED);
+        ui.fillRect(10,25,20,100);
+    }
+}*/
+
+
